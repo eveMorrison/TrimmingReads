0ce9d5004a771cb0928b1ac68602443ff8ef3b68
Eva Morrison <eva.morrison33@ku.edu>
2022-08-30 12:35:56 -0500

    create flank extraction program in file iteration

--- fileIteration.py
+++ fileIteration.py
@@ -1,19 +1,41 @@
 import re
 
-trimming_info_File = open("reads_to_trim_dys.txt", "r")
+fasta_file = "Dys_reads_to_trim.fasta"
+csv_file = "reads_to_trim_dys.txt"
+
+def find_fa_seq(fname, seq_ID):
+   with open(fname, "r") as fh:
+      identifier = None
+      sequence = []
+      for line in fh:
+         line = line.strip()  # Remove trailing newline characters.
+         if line.startswith(">"):
+            identifier = line
+            print(identifier)
+            sequence = []
+         else:
+            sequence.append(line)
+
+trimming_info_File = open(csv_file, "r")
 
 for line in trimming_info_File:
-   if re.search('^.+\d',line,0):
+   if re.search('^>',line,0):
       values = line.split()
       id_dys = values[0]
-      leng_dys = values[1]
-      leng_split = values[5]
+      leng_dys = int(values[1])
+      leng_flank = float(values[5])
+      sequence_start = leng_flank
+      sequence_end = leng_dys - leng_flank
+
+      find_fa_seq(fasta_file,id_dys)
       #print(id_dys)
       #print(leng_dys)
-      #print(leng_split)
+      #print(leng_flank)
+      #print(sequence_start)
+      #print(sequence_end)
 
-
-linelist = trimming_info_File.readlines()
-fileLeng = len(linelist)
+#get information on how long the file is
+#linelist = trimming_info_File.readlines()
+#fileLeng = len(linelist)
  
 trimming_info_File.close()
