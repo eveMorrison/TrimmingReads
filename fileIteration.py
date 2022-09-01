d8bf7eef12742a0eae4682ee532ec0927fb7c567
Eva Morrison <eva.morrison33@ku.edu>
2022-09-01 15:40:31 -0500

    update files

--- fileIteration.py
+++ fileIteration.py
@@ -1,22 +1,32 @@
+from posixpath import split
 import re
+import math
+from pprint import pprint
 
 fasta_file = "Dys_reads_to_trim.fasta"
 csv_file = "reads_to_trim_dys.txt"
 
-def find_fa_seq(fname, seq_ID):
+def find_fa_seq(fname, seq_ID, seq_len, seq_start, seq_end):
    with open(fname, "r") as fh:
       identifier = None
       sequence = []
       for line in fh:
          line = line.strip()  # Remove trailing newline characters.
          if line.startswith(">"):
-            identifier = line
-            print(identifier)
+            identifier = line.split()[0]
+            #print(identifier)
             sequence = []
          else:
             sequence.append(line)
+            sequence_str = "".join([str(item) for item in sequence])
+            if(identifier == seq_ID and len(sequence_str) >= seq_len):
+               #print(len(sequence_str))
+               #print(sequence_str[seq_start:seq_end+1])
+               f.write(seq_ID + "\n")
+               f.write(sequence_str[seq_start:seq_end+1] + "\n")
 
 trimming_info_File = open(csv_file, "r")
+f = open("dys_reads_trimmed.fasta", "a")
 
 for line in trimming_info_File:
    if re.search('^>',line,0):
@@ -24,10 +34,10 @@ for line in trimming_info_File:
       id_dys = values[0]
       leng_dys = int(values[1])
       leng_flank = float(values[5])
-      sequence_start = leng_flank
-      sequence_end = leng_dys - leng_flank
+      sequence_start = math.ceil(leng_flank)
+      sequence_end = leng_dys - math.floor(leng_flank)
 
-      find_fa_seq(fasta_file,id_dys)
+      find_fa_seq(fasta_file,id_dys,leng_dys, sequence_start, sequence_end)
       #print(id_dys)
       #print(leng_dys)
       #print(leng_flank)
@@ -38,4 +48,5 @@ for line in trimming_info_File:
 #linelist = trimming_info_File.readlines()
 #fileLeng = len(linelist)
  
+f.close() 
 trimming_info_File.close()
